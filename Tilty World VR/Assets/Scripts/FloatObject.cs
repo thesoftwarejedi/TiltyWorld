29d37dadf46504e5472c9a4178ab745697670fb8
Dana Hanna <dana.hanna@gmail.com>
2016-12-29 17:21:32 -0500

    mechanics

---
+++ Tilty World VR/Assets/Scripts/FloatObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatObject : MonoBehaviour
+{
+    public GameObject ObjectToFloat;
+    public float UnitsPerSecond = 3;
+    public bool Active = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Active)
+        {
+            ObjectToFloat.transform.Translate(Vector3.up * Time.deltaTime);
+        }
+    }
+}
---
+++ Tilty World VR/Assets/Scripts/GameplayEngine.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayEngine : MonoBehaviour {
+
+    public float SecondsUntilFloat = 10f;
+    public float SecondsUntilFloatStop = 20f;
+    public float SecondsUntilSpin = 22f;
+
+    private DateTime _startTime;
+    private RotateObject _rotateObject;
+    private FloatObject _floatObject;
+
+	// Use this for initialization
+	void Start () {
+        _startTime = DateTime.Now;
+        _rotateObject = FindObjectOfType<RotateObject>();
+        _floatObject = FindObjectOfType<FloatObject>();
+
+
+        Invoke("BeginFloating", SecondsUntilFloat);
+        Invoke("StopFloating", SecondsUntilFloatStop);
+        Invoke("BeginSpinning", SecondsUntilSpin);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void BeginFloating()
+    {
+        _floatObject.Active = true;
+    }
+
+    void StopFloating()
+    {
+        _floatObject.Active = false;
+    }
+
+    void BeginSpinning()
+    {
+        _rotateObject.Active = true;
+    }
+}
